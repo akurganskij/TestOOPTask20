ae2476fce947cc826527ab48a8aa40bbcda9f525
akurganskij <78171489+akurganskij@users.noreply.github.com>
2022-06-08 11:53:35 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp49/Form1.cs
@@ -0,0 +1,29 @@
+namespace WinFormsApp49
+{
+    public partial class Form1 : Form
+    {
+        private int clicked = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            TicTacToeButton [] buttons = new TicTacToeButton [9];
+            for(int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i] = new TicTacToeButton(100) { Location = new Point(100 + 150 *(i % 3), 50 + 150 * (i / 3))};
+                buttons[i].Click += Button1_Click;
+            }
+            this.Controls.AddRange(buttons);
+        }
+
+        private void Button1_Click(object? sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty((sender as TicTacToeButton).Text))
+            {
+                if (clicked % 2 == 0) (sender as TicTacToeButton).ToCross();
+                else (sender as TicTacToeButton).ToNull();
+                clicked++;
+            }
+        }
+    }
+}
---
+++ WinFormsApp49/TicTacToeButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp49
+{
+    public class TicTacToeButton : Button
+    {
+        public TicTacToeButton(int size)
+        {
+            this.Width = size;
+            this.Height = size;
+        }
+
+        public void ToCross()
+        {
+            this.Text = "X";
+        }
+        public void ToNull()
+        {
+            this.Text = "O";
+        }
+        
+        new public int Size { get { return this.Width; } }
+    }
+}
